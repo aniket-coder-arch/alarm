f5d0480be2f01e84e989a73e3e3516df9bed581d
Aniket Dubey <67035388+aniket-coder-arch@users.noreply.github.com>
2021-03-04 12:26:31 +0530

    Update alarm.py

--- alarm.py
+++ alarm.py
@@ -2,16 +2,50 @@ import time
 import datetime
 import pyttsx3
 import pyaudio
+import speech_recognition as sr
+
+
+engine = pyttsx3.init('sapi5')
+voices = engine.getProperty('voices')
+engine.setProperty('voice', voices[1].id)
+engine.setProperty('rate',180)
+
+
+def speak(audio): 
+	engine.say(audio) 
+	engine.runAndWait()
+
+
+def takeCommand():
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        print("Listening...")
+        r.pause_threshold = 1
+        audio = r.listen(source,phrase_time_limit=5)
+    try:
+        print("Recognizing...")    
+        query = r.recognize_google(audio, language='en-in')
+        print(f"User said: {query}\n")
+    except Exception as e:
+        # print(e)    
+        print("Say that again please...")  
+        return "None"
+    return query
 
 
 def alarm():
     try:
-        Name = input("Enter your name: ")
+        speak("Enter your name: ")
+        Name = takeCommand()
         now = datetime.datetime.now()
         current_time = now.strftime("%I:%M:%S")
         print("Current Time is: ", current_time)
-        Time1=input("Enter the hour (HH): ")
-        Time2=input("Enter the minute (MM): ")
+        print("Enter the hour (HH):")
+        speak("Enter the hour")
+        Time1=takeCommand()
+        print("Enter the minute (MM):")
+        speak("Enter the minute")
+        Time2=takeCommand()
         Time=Time1+":"+Time2
         while True:
             Standard_time=datetime.datetime.now().strftime("%I:%M")
@@ -20,11 +54,7 @@ def alarm():
                 count=0
                 while count<=10:
                     count=count+1
-                    # creating a engine for voice mode
-                    engine=pyttsx3.init()
-                    # setting the voice
-                    engine.say("Wake up"+Name)
-                    engine.runAndWait()
+                    speak("Wake up"+Name)
                 print("Thankyou For using the Interface")
                 break
     except Exception as e:
